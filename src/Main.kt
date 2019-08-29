92ce4f576cada741cb5844fcb190b107cea8a617
shinyay <shinya.com@gmail.com>
2019-08-30 07:29:17 +0900

    Add flatMap which decompose dimensional list and compose flat list

--- src/Main.kt
+++ src/Main.kt
@@ -1,18 +1,26 @@
 fun main() {
     val listInt = (1..10).toList()
 
-    println("map: list.map { num -> \"Num: \$num\"}")
+    println("[map]: list.map { num -> \"Num: \$num\"}")
     callAction{ map(listInt) }
 
-    println("mapIndexes: list.mapIndexed { index, num -> \"Index:\$index - \$num\" }")
+    println("[mapIndexes]: list.mapIndexed { index, num -> \"Index:\$index - \$num\" }")
     callAction { mapIndexed(listInt) }
 
-    println("mapNotNull: list.mapNotNull { num -> if (num == null) null else \"Num: \$num\" }")
+    println("[mapNotNull]: list.mapNotNull { num -> if (num == null) null else \"Num: \$num\" }")
     callAction{ mapNotNull(listOf(1,null,3,null,5)) }
+
+    println("[flatMap]: list.flatMap { num -> num }")
+    callActionInt { flatMap(listOf((0..4).toList(), (5..9).toList())) }
 
 }
 
 fun callAction(function: () -> List<String?>) {
+    println(function())
+    println("--------------------------------------------")
+}
+
+fun callActionInt(function: () -> List<Int>) {
     println(function())
     println("--------------------------------------------")
 }
